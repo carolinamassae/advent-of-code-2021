e8911bda88431160b53ba82ca6baa05ee869fe26
Carolina Massae <carolinamassae@gmail.com>
2021-12-03 14:13:57 +0100

    Adding new rules to gitignore and reformat day1

--- Day1.py
+++ Day1.py
@@ -1,5 +1,6 @@
 from itertools import islice, tee
 
+
 def main():
     with open('Input/day1.txt') as f:
         lines = [int(l.strip('\n\r')) for l in f.readlines()]
@@ -8,7 +9,7 @@ def main():
     print(compare_block(lines))
     print(compare_block_pedro(lines, 3))
 
-        
+
 def compare_number(lines):
     count = 0
     for previous, current in zip(lines, lines[1:]):
@@ -20,15 +21,16 @@ def compare_number(lines):
 def compare_block(lines):
     count = 0
     for n1, n2, n3, n4 in zip(lines, lines[1:], lines[2:], lines[3:]):
-        if n1+n2+n3 < n2+n3+n4:
+        if n1 + n2 + n3 < n2 + n3 + n4:
             count += 1
     return count
 
 
 def sliding_window(iterable, size):
     iterables = tee(iter(iterable), size)
-    window = zip(*(islice(t, n, None) for n,t in enumerate(iterables)))
+    window = zip(*(islice(t, n, None) for n, t in enumerate(iterables)))
     yield from window
+
 
 def compare_block_pedro(lines, n):
     count = 0
